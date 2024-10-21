ddbce221c0165f9c75050912de6fb612e84262dc
yeon-dong <jae073@naver.com>
2024-10-21 17:10:06 +0900

    마이룸 페이지 UI 제작 중(디자인 수정 필요 확인중)

--- TinyRoom_FE/Tiny-Room-FE/src/pages/UserMainPage/UserMainPage.style.js
+++ TinyRoom_FE/Tiny-Room-FE/src/pages/UserMainPage/UserMainPage.style.js
@@ -3,6 +3,7 @@ import { styled } from "styled-components";
 export const Container = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   position: absolute;
   top: 0;
   bottom: 0;
