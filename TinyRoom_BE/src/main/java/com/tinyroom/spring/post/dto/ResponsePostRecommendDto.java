ad6d375dd8ed205ad54df95a0c6a50a7da7a0b04
inwoo0808 <inwoo0808@gmail.com>
2024-10-24 16:23:22 +0900

    post recommend 구현 완료

---
+++ TinyRoom_BE/src/main/java/com/tinyroom/spring/post/dto/ResponsePostRecommendDto.java
@@ -0,0 +1,25 @@
+package com.tinyroom.spring.post.dto;
+
+import com.tinyroom.spring.category.dto.CategoryDto;
+import com.tinyroom.spring.post.domain.Post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter
+@Getter
+@ToString
+@Builder
+public class ResponsePostRecommendDto {
+	private int post_id;
+	private String title;
+	private String thumbnail;
+	private String text_content;
+	private String content;
+	private String post_img;
+	private int heartCount;
+	private int commentCount;
+}
