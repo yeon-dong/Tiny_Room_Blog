7f940cf3c53ac4efa4aab739fbc105ba21cd38b9
yeon-dong <jae073@naver.com>
2024-10-25 11:02:42 +0900

    이웃 추가 모달 구현 중

--- TinyRoom_FE/Tiny-Room-FE/src/pages/UserMainPage/UserInfoBox.jsx
+++ TinyRoom_FE/Tiny-Room-FE/src/pages/UserMainPage/UserInfoBox.jsx
@@ -3,13 +3,34 @@ import {
   Container,
   DescriptionBox,
   EditButton,
+  NeighborButton,
   ProfileBox,
   ProfileImageBox,
   UsernameBox,
 } from "./UserInfoBox.style";
+import useStore from "../../stores/store";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import NeighborModal from "../../components/NeighborModal/NeighborModal";
 
-const UserInfoBox = ({ userId, profileImg, nickname, description }) => {
-  const at = localStorage.getItem("at");
+const UserInfoBox = ({
+  userId,
+  profileImg,
+  nickname,
+  description,
+  blogData,
+}) => {
+  const { id, postId } = useParams();
+  const { userInfo } = useStore();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const isLogin = userInfo != null; // 로그인 되어있으면 true 반환
+  const isMyRoom = isLogin ? userInfo.id == id : false; // 로그인 id와 path 같으면 true반환
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <Container>
@@ -19,13 +40,28 @@ const UserInfoBox = ({ userId, profileImg, nickname, description }) => {
         </ProfileImageBox>
         <UsernameBox>
           {nickname}
-          {at !== null && (
-            <EditButton to={`/${userId}/mypage`}>Edit</EditButton>
+          {isLogin ? (
+            isMyRoom ? (
+              <EditButton to={`/${userId}/mypage`}>Edit</EditButton>
+            ) : (
+              <NeighborButton onClick={() => handleModalOpen()}>
+                이웃추가
+              </NeighborButton>
+            )
+          ) : (
+            <></>
           )}
         </UsernameBox>
         <DescriptionBox>{description}</DescriptionBox>
       </ProfileBox>
       <Calendar />
+      {isModalOpen && (
+        <NeighborModal
+          handleModalClose={handleModalClose}
+          nickname={nickname}
+          blogData={blogData}
+        />
+      )}
     </Container>
   );
 };
