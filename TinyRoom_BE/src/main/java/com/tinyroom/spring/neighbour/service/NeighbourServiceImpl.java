3375d236a4d51596718687e4cde1dbead11333cb
inwoo0808 <inwoo0808@gmail.com>
2024-10-25 10:24:44 +0900

    neighbour send List 구현중

--- TinyRoom_BE/src/main/java/com/tinyroom/spring/neighbour/service/NeighbourServiceImpl.java
+++ TinyRoom_BE/src/main/java/com/tinyroom/spring/neighbour/service/NeighbourServiceImpl.java
@@ -3,12 +3,17 @@ package com.tinyroom.spring.neighbour.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.tinyroom.spring.blog.dto.PostPageDto;
 import com.tinyroom.spring.member.domain.Member;
 import com.tinyroom.spring.neighbour.dao.NeighbourDao;
 import com.tinyroom.spring.neighbour.domain.Neighbour;
 import com.tinyroom.spring.neighbour.dto.NeighbourDto;
+import com.tinyroom.spring.neighbour.dto.NeighbourPageDto;
 import com.tinyroom.spring.post.service.PostServiceImpl;
 
 import lombok.extern.log4j.Log4j2;
@@ -50,5 +55,18 @@ public class NeighbourServiceImpl implements NeighbourService{
 	@Override
 	public List<Neighbour> getNeighbourList(Member member) {
 		return neighbourDao.findByMember(member);
+	}
+
+	@Override
+	public List<NeighbourPageDto> getSendNeighbourList(Member member, int page) {
+		Pageable pageable = PageRequest.of(page, 4, Sort.Direction.DESC, "neighbour_id");
+		org.springframework.data.domain.Page<NeighbourPageDto> neighbourPage = neighbourDao.findByConditions(member, pageable);
+		return null;
+	}
+
+	@Override
+	public int countSendNeighbour(Member member) {
+		// TODO Auto-generated method stub
+		return neighbourDao.countByConditions(member);
 	}
 }
