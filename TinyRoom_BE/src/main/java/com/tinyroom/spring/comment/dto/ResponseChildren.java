84c70cbc50e2349d4f926d859cae08c172bb5063
itcuna0617 <itcuna0617@gmail.com>
2024-10-25 16:32:39 +0900

    댓글 페이지네이션에서 필요한 정보 추가 #126

--- TinyRoom_BE/src/main/java/com/tinyroom/spring/comment/dto/ResponseChildren.java
+++ TinyRoom_BE/src/main/java/com/tinyroom/spring/comment/dto/ResponseChildren.java
@@ -1,5 +1,8 @@
 package com.tinyroom.spring.comment.dto;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +13,20 @@ import lombok.Setter;
 @Setter
 public class ResponseChildren {
 	private int commentId;
+	private String username;
 	private String content;
+	private String date;
 	private int postId;
 	private Integer parentId;
 	
-	public ResponseChildren(Integer commentId, String content, Integer postId, Integer parentId) {
+	public ResponseChildren(Integer commentId, String nickname, String content, Integer postId, LocalDate date, Integer parentId) {
 		this.commentId = commentId;
 		this.content = content;
 		this.postId = postId;
 		this.parentId = parentId;
+		this.username = nickname;
+//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//		this.date = format.format(date);
+		this.date = date.toString();
 	}
 }
--- TinyRoom_BE/src/main/java/com/tinyroom/spring/comment/dto/ResponseCommentListDto.java
+++ TinyRoom_BE/src/main/java/com/tinyroom/spring/comment/dto/ResponseCommentListDto.java
@@ -1,5 +1,7 @@
 package com.tinyroom.spring.comment.dto;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +16,22 @@ import lombok.Setter;
 @Setter
 public class ResponseCommentListDto {
 	private int commentId;
+	private String username;
 	private String content;
 	private int postId;
+	private String date;
 	private Integer parentId;
 	private List<ResponseChildren> children;
 	
-	public ResponseCommentListDto(Integer commentId, String content, Integer postId, Integer parentId) {
+	public ResponseCommentListDto(Integer commentId, String nickname, String content, Integer postId, LocalDate date, Integer parentId) {
 		this.commentId = commentId;
 		this.content = content;
 		this.postId = postId;
 		this.parentId = parentId;
+		this.username = nickname;
+//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//		this.date = format.format(date);
+		this.date = date.toString();
 		this.children = new ArrayList<>();
 	}
 }
